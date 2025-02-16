1cf40456cfef3fc5b1dfeeeea9b7990fdac4404e
Luis Alberto Castro Cruz <157860624+Wicho1233@users.noreply.github.com>
2025-02-15 23:37:36 -0600

    Add files via upload

---
+++ Strings.py
@@ -0,0 +1,15 @@
+texto = "Estamos programando con python"
+#Manera de imprimir un elemento de un texto de acuerdo al numero
+#Se imprimira la primera letra 
+print(texto[0])
+#Manera de imprimir elementos de manera inversa
+#Se imprimira la ultima letra del String
+print(texto[-10])
+#Manera de imprimir un rango de elementos
+# Se imprimira solo el rango seleccionado 
+print(texto[0:7])
+#Manera de imprimir solo un numero de terminado de numeros
+#Iniciando de 0 : Numero de veces que se va imprimir:cada cuanto va a imprimir elementos
+print(texto[0:10:2])
+#Manera de imprimir una palabra de manera inversa
+print(texto[::-1])
