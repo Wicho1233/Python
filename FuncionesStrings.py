04cc9fffc9986d4a8af4ff8e264276777fd8f3ec
Luis Alberto Castro Cruz <157860624+Wicho1233@users.noreply.github.com>
2025-02-17 02:05:48 -0600

    Add files via upload

---
+++ FuncionesStrings.py
@@ -0,0 +1,13 @@
+texto = "Estamos trabajando en Python"
+
+#Devuelve cantidad de elementos que tiene el String
+print(len(texto))
+
+
+#Convierte la primera letra en mayuscula
+print(texto.capitalize())
+
+#Cambia palabras en el string se coloca la palabra del String, separado por una 
+#coma la palabra or la que se desea remplazar
+
+print(texto.replace("python", "Java"))
