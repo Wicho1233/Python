a77691726480ff4ffec4b26cc4ed88133d4e463d
Luis Alberto Castro Cruz <157860624+Wicho1233@users.noreply.github.com>
2025-02-18 22:14:43 -0600

    Add files via upload

---
+++ FuncionesNumeros.py
@@ -0,0 +1,17 @@
+from math import *
+
+num = 2
+num2 =3
+num3 =15
+#Valor absoluto de un numero
+print(abs(num))
+#Potencia de un numero
+print(pow(num,2))
+#Maximo de una lista de numeros
+print(max(num2,num3))
+#Minimo de los numero ingresados
+print(min(num2,num3))
+#Redondeo de numeros
+print(round(4.4))
+
+
