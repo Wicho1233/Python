feaa6baa5bb7785f89a8922b8311c038ae3f8ab7
Luis Alberto Castro Cruz <157860624+Wicho1233@users.noreply.github.com>
2025-02-23 22:10:43 -0600

    Funciones para modificar diccionarios

---
+++ ModificarDiccionario.py
@@ -0,0 +1,17 @@
+alumnos ={'Juan':23,'Pedro':26,'Laura':24}
+#Imprimir un valor del diccionario
+#Editar valors
+alumnos['Juan']=24
+print(alumnos['Juan'])
+
+#Definir elementos
+alumnos['Pablo']=21
+print(alumnos)
+
+#Eliinar elementos
+del alumnos['Juan']
+print(alumnos)
+
+#Reviar si la clave esta dentro del diccionario
+print('Pedro' in alumnos)
+print('Eduardo' in alumnos)
