6cbffb0d4fdecf990b844e07717dfd31c7a78026
Luis Alberto Castro Cruz <157860624+Wicho1233@users.noreply.github.com>
2025-02-24 23:43:11 -0600

    Sentencia condicional if

---
+++ if.py
@@ -0,0 +1,16 @@
+num1 =7
+num2 =7
+num3 =8
+if num1 >=num2 and num1>=num3:
+    print('El numero '+str(num1)+' es mayor')
+elif num1<=num2 and num2>=num3:
+    print('El numero '+str(num2)+' es mayor') 
+elif num3 >=num2 and num3>=num3:
+    print('El numero '+str(num3)+' es mayor')
+else:
+   print('Los numeros son iguales')
+
+
+
+
+    
