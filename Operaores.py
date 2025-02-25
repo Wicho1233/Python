4da0058bc14832678362f4abe18c7524dca055e5
Luis Alberto Castro Cruz <157860624+Wicho1233@users.noreply.github.com>
2025-02-24 23:30:12 -0600

    Operadores y su funcion en python

---
+++ Operaores.py
@@ -0,0 +1,19 @@
+#Devuelve datos bookeanos
+print (5>2)
+
+print(1<3)
+
+print(8 == 8)
+print(8 != 8)
+
+#Se puede trabajar con Strings
+print('A'>'B')
+
+#Operador and
+print((5<4)and(1>2))
+
+#Operador or
+print((5<4)or(1>2))
+
+#operador not
+print(not(3>2))
