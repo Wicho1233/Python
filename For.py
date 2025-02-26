b507aa4c867b240260416c6cff99f57386930601
Luis Alberto Castro Cruz <157860624+Wicho1233@users.noreply.github.com>
2025-02-25 23:56:02 -0600

    Ejemplo de for en python

---
+++ For.py
@@ -0,0 +1,14 @@
+#Son estructuras de control que permien ejecutar una accion en varias ocaciones
+
+numeros = (2,3,5,7,9,11,13,17,19)
+
+for i in range(10):
+   print(i)
+   print('iteracion completa')
+
+for i in numeros:
+   print(i)   
+
+for i in range(len(numeros)):
+   print(numeros[i])
+
