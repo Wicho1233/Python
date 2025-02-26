1b9928a5ba6288967c4d4eb13c484b50aec5559d
Luis Alberto Castro Cruz <157860624+Wicho1233@users.noreply.github.com>
2025-02-25 23:48:17 -0600

    switch con strings en python

---
+++ match.py
@@ -0,0 +1,31 @@
+
+
+mes = 'Enero'
+
+match mes:
+    case 'Enero':
+        print('Enero es el primer mes ')
+    case 'Febrero':
+        print('Febrero es el segundo mes ')  
+    case 'Marzo':
+        print('marzo es el tercer mes ')
+    case 'Abril':
+        print('Abril es el cuarto mes ')
+    case 'Mayo':
+        print('Mayo es el quinto mes ')
+    case 'Junio':
+        print('Junio es el sexto mes ')  
+    case 'Julio':
+        print('Julio es el septimo mes ')
+    case 'Agosto':
+        print('Agosto es el octavo mes ')  
+    case 'Septiembre':
+        print('Septiembre es el noveno mes ')
+    case 'Octubre':
+        print('Octubre es el decimo mes ')  
+    case 'Noviembre':
+        print('Noviembre es el decimo primero mes ')
+    case 'Diciembre':
+        print('Diciembre es el decimo segundo mes ')  
+
+
