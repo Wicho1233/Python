f2da9b67d33624434de66b3e8112f10e84226890
Luis Alberto Castro Cruz <157860624+Wicho1233@users.noreply.github.com>
2025-02-27 22:34:52 -0600

    Sentencia return en funciones

---
+++ SentenciaReturn.py
@@ -0,0 +1,17 @@
+
+def saludo(nombre,edad):
+    print('Te llamas '+nombre+' y tienes '+(edad))
+
+
+def valor_medio(num1,num2):
+    media =(num1+num2)/2
+    return media
+
+
+
+
+ValorMedio = valor_medio(8,4)
+ValorMedio *= 5
+print(ValorMedio)
+
+
