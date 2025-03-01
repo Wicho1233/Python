2fcf45f669a45e6cae586d52f8410df35ca948de
Luis Alberto Castro Cruz <157860624+Wicho1233@users.noreply.github.com>
2025-02-28 22:00:58 -0600

    Funcion para escribir en archivos

---
+++ EscribirArchivos.py
@@ -0,0 +1,13 @@
+
+#En caso de no tener el archivo txt se creara en la capeta
+#w da referencia  write pra escribir en el archivo
+archivo =open("alumnos2.txt","w")
+
+archivo.write("\nPedro")
+
+archivo.close()
+
+
+
+
+
