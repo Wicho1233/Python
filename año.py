66034344fc38c2459eba964a23ff41c34b24b6fd
Luis Alberto Castro Cruz <157860624+Wicho1233@users.noreply.github.com>
2025-03-11 22:26:59 -0600

    Uso de la libreria datatime

---
+++ año.py
@@ -0,0 +1,20 @@
+
+
+import datetime
+fecha=datetime.datetime.now()
+print(fecha)
+
+import datetime
+año=datetime.datetime.strftime( fecha,"%Y")
+print(año)
+
+
+
+
+
+#%d Dia
+#%m Mes
+#%Y Año
+#%H Hora
+#%M Minutos
+#%s Segundos
