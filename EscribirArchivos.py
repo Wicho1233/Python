913106857d7ceb6c2bdf7282c1a3df4cce1e1f25
Luis Alberto Castro Cruz <157860624+Wicho1233@users.noreply.github.com>
2025-03-19 15:44:35 -0600

    Update EscribirArchivos.py

--- EscribirArchivos.py
+++ EscribirArchivos.py
@@ -2,7 +2,7 @@
 #En caso de no tener el archivo txt se creara en la capeta
 #w da referencia  write pra escribir en el archivo
 archivo =open("alumnos2.txt","w")
-
+#Forma de escribir en un archivo txt
 archivo.write("\nPedro")
 
 archivo.close()
